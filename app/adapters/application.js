b186d1741d06d6c65626e11f395daaf049f98a86
byelipk <byelipk@gmail.com>
2017-06-26 14:27:45 -0400

    Pluralize and underscore model names

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,12 @@
 // export { default } from 'ember-local-storage/adapters/local';
 
 import DS from 'ember-data';
+import Inflector from 'ember-inflector';
 
 export default DS.JSONAPIAdapter.extend({
-  // Application specific overrides go here
+  namespace: 'api/v1',
+
+  pathForType: function(type) {
+    return Inflector.pluralize(Ember.String.underscore(type));
+  }
 });
